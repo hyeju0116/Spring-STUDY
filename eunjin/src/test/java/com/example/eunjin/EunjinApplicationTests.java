9de65e3073fed74f5b4e7ddcf21856957be1c662
정은진 <jeong-eunjin@eunjinMac.local>
2025-07-01 19:43:12 +0900

    branch test

---
+++ eunjin/src/test/java/com/example/eunjin/EunjinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eunjin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EunjinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
