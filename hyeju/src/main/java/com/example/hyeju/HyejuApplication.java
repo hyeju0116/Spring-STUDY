5e9b241cba56141218ab3db0d3fce95e86c1b8b1
CholHyeJu <cholhyeju123@gmail.com>
2025-07-14 16:45:02 +0900

    test/hyeju

---
+++ hyeju/src/main/java/com/example/hyeju/HyejuApplication.java
@@ -0,0 +1,14 @@
+package com.example.hyeju;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HyejuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HyejuApplication.class, args);
+		System.out.println("Hello, Hyeju!");
+	}
+
+}
