e13faf1a8186f24f77d80c941e1df00ec2f67b4e
CholHyeJu <cholhyeju123@gmail.com>
2025-07-20 22:54:16 +0900

    "week03 실습 - 롬복과 리팩터링"

--- hyeju/src/main/java/com/example/hyeju/controller/entity/Article.java
+++ hyeju/src/main/java/com/example/hyeju/controller/entity/Article.java
@@ -4,9 +4,12 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
 
 @Entity
-
+@AllArgsConstructor
+@ToString
 public class Article {
     @Id
     @GeneratedValue
@@ -16,18 +19,5 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long id, String title, String content) { //아래는 모두 DTO에서 했던 것처럼 생성자를 만드는 것이다 이것은 DTO로 이동을 시킬 때 사용
-        this.id = id;
-        this.title = title;
-        this.content = content;
-    }
 
-    @Override
-    public String toString() {
-        return "Article{" +
-                "id=" + id +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
