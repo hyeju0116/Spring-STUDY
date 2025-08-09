e3d900234fbe5bc74f9ba36f5c6f87831bbffd0b
hyeju0116 <cholhyeju123@gmail.com>
2025-08-09 21:27:52 +0900

    "week08 실습 - CRUD와 SQL 쿼리 종합"

--- eunjin/src/main/java/com/example/eunjin/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.eunjin.repository;
-
-import com.example.eunjin.entity.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-}
--- hyeju/src/main/java/com/example/hyeju/controller/entity/Article.java
+++ hyeju/src/main/java/com/example/hyeju/controller/entity/Article.java
@@ -1,9 +1,6 @@
 package com.example.hyeju.controller.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,8 +13,8 @@ import lombok.ToString;
 @Getter
 public class Article {
     @Id
-    @GeneratedValue
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //DB가 id 자동 생성
+    private Long id; //article에서 기본키는 id
     @Column
     private String title;
     @Column
